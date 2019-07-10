b1f7e032d1644d727c32929cd2f9c6d5d112b3c9
DiogoBessa <diogobessac@gmail.com>
2019-07-10 10:55:58 +0100

    config spring

--- agenda-digital-parent/agenda-digital-client/src/main/java/com/everis/academia/java/agenda/digital/web/soap/CreateClient.java
+++ agenda-digital-parent/agenda-digital-client/src/main/java/com/everis/academia/java/agenda/digital/web/soap/CreateClient.java
@@ -1,19 +1,19 @@
 package com.everis.academia.java.agenda.digital.web.soap;
 
-import java.net.URL;
+//import java.net.URL;
 
 
-public class CreateClient {
-
-	public static void main(String[] args) {
-		URL url = new URL("http://localhost:8080/agenda-digital-web/soap/CidadeSOAP?wsdl");
-
-		CidadeSOAPService service = new CidadeSOAPService(url);
-		CidadeSOAP port = service.getCidadeSOAPPort();
-
-		for (Cidade cidade : port.read()) {
-			System.out.println(cidade.getNome());
-		}
-	}
-
-}
+//public class CreateClient {
+//
+//	public static void main(String[] args) {
+//		URL url = new URL("http://localhost:8080/agenda-digital-web/soap/CidadeSOAP?wsdl");
+//
+//		CidadeSOAPService service = new CidadeSOAPService(url);
+//		CidadeSOAP port = service.getCidadeSOAPPort();
+//
+//		for (Cidade cidade : port.read()) {
+//			System.out.println(cidade.getNome());
+//		}
+//	}
+//
+//}
